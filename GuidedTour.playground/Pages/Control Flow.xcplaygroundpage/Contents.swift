e0bdcd84134332090878e5e416fdecdc25f98f46
Sen <slboat@me.com>
2015-11-29 17:39:49 +0800

    * 2015-11-29 练习

--- GuidedTour.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
+++ GuidedTour.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
@@ -57,7 +57,7 @@ switch vegetable { //开始切换开关
         //默认不会往下,不用break
     case "cucumber", "watercress": //这是两种字符串对比
         print("That would make a good tea sandwich.")
-    case let x where x.hasSuffix("pepper"): //字符串拆包,并且判断前缀
+    case let x where x.hasSuffix("pepper"): //字符串拆包,并且判断前缀-可选值绑定
         print("Is it a spicy \(x)?")
     default: //默认的玩意,必须有它...
         print("Everything tastes good in soup.")
@@ -73,33 +73,44 @@ switch vegetable { //开始切换开关
 //: You use `for`-`in` to iterate over items in a dictionary by providing a pair of names to use for each key-value pair. Dictionaries are an unordered collection, so their keys and values are iterated over in an arbitrary order.
 //:
 let interestingNumbers = [
-    "Prime": [2, 3, 5, 7, 11, 13],
-    "Fibonacci": [1, 1, 2, 3, 5, 8],
-    "Square": [1, 4, 9, 16, 25],
+    "Prime": [2, 3, 5, 7, 11, 13], //字典的键值和值
+    "Fibonacci": [1, 1, 2, 3, 5, 8],//数组只能是一个类型?
+    "Square": [1, 4, 9, 16, 25], //字典的键和值,屁股后面可以跟个逗号
 ]
-var largest = 0
-for (kind, numbers) in interestingNumbers {
-    for number in numbers {
-        if number > largest {
+
+var largest = 0 //最大的标记
+var largestKind = ""
+for (kind, numbers) in interestingNumbers { //遍历所有的数字
+    for number in numbers { //遍历内部的
+        if number > largest { //对比值..看起来它得自己相信它可以是数字的
             largest = number
+            largestKind = kind
         }
     }
 }
-print(largest)
+print("\(largest) in \(largestKind)")
 
+
+//数组类型推测
+//let d = [1,2,"3"] //出错,可怜的人,没有Swift类型指定,也没有Foundation撑腰
+
+//import Foundation;
+//let c = [1,2,"3"] //必须导入这个Foundation才能拥有混合类型.
+
+let b:[Any] = [1,2,"3"] //这是特别指定类型,Swift家的
 //: > **Experiment**:
 //: > Add another variable to keep track of which kind of number was the largest, as well as what that largest number was.
 //:
 //: Use `while` to repeat a block of code until a condition changes. The condition of a loop can be at the end instead, ensuring that the loop is run at least once.
 //:
 var n = 2
-while n < 100 {
+while n < 100 { //标准while,while和花括号,不确定会否执行一次,但是总会回来...
     n = n * 2
 }
 print(n)
 
 var m = 2
-repeat {
+repeat { //倒置的while,至少总是有一遍会执行
     m = m * 2
 } while m < 100
 print(m)
@@ -107,13 +118,13 @@ print(m)
 //: You can keep an index in a loop—either by using `..<` to make a range of indexes or by writing an explicit initialization, condition, and increment. These two loops do the same thing:
 //:
 var firstForLoop = 0
-for i in 0..<4 {
+for i in 0..<4 { //新型的循环...点点小于
     firstForLoop += i
 }
 print(firstForLoop)
 
 var secondForLoop = 0
-for var i = 0; i < 4; ++i {
+for var i = 0; i < 4; ++i { //旧时代的循环,三段,接括号也可以 for (var i = 0; i < 4; ++i){}
     secondForLoop += i
 }
 print(secondForLoop)
